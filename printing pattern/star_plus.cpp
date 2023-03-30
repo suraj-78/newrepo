ee728d242b57833bda3474c8c2698db20ffb00a4
suraj-78 <surajpandit591@gmail.com>
2023-03-31 03:57:27 +0530

    optimised way of star plus pattern

--- printing pattern/star_plus.cpp
+++ printing pattern/star_plus.cpp
@@ -4,17 +4,26 @@ int main(){
     int n;
     cout<<"Enter the value of n";
     cin>>n;
-    for(int i=1; i<=n;i++){
-        if(i == n/2 + 1){
-            for(int i = 1; i<= n ; i++){
+    // for(int i=1; i<=n;i++){
+    //     if(i == n/2 + 1){
+    //         for(int i = 1; i<= n ; i++){
+    //             cout<<"*";
+    //         }
+    //     }
+    //     else{
+    //         for(int k = 1; k<=(n-1)/2; k++){
+    //             cout<<" ";
+    //         }
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    // }
+    for(int i=1; i<=n ; i++){
+        for(int j =1 ; j<=n ; j++){
+            if(i ==((n/2)+1) || j==n/2+1){
                 cout<<"*";
             }
-        }
-        else{
-            for(int k = 1; k<=(n-1)/2; k++){
-                cout<<" ";
-            }
-            cout<<"*";
+            else cout<<" ";
         }
         cout<<endl;
     }
