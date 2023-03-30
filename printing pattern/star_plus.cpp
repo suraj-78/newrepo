7b6c6140c33d4e5dd8190cea683d12f59290c8c8
suraj-78 <surajpandit591@gmail.com>
2023-03-31 03:36:54 +0530

    star plus pattern

--- printing pattern/star_plus.cpp
+++ printing pattern/star_plus.cpp
@@ -5,8 +5,16 @@ int main(){
     cout<<"Enter the value of n";
     cin>>n;
     for(int i=1; i<=n;i++){
-        for(int j =1; j<=n; j++){
-            cout<<j;
+        if(i == n/2 + 1){
+            for(int i = 1; i<= n ; i++){
+                cout<<"*";
+            }
+        }
+        else{
+            for(int k = 1; k<=(n-1)/2; k++){
+                cout<<" ";
+            }
+            cout<<"*";
         }
         cout<<endl;
     }
