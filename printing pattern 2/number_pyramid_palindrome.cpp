f1abae746b02510cc281d6634af4be068de8eab3
suraj-78 <surajpandit591@gmail.com>
2023-04-02 14:09:56 +0530

    number_pyramid_palindrome

---
+++ printing pattern 2/number_pyramid_palindrome.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the vlaue of n";
+    cin>>n;
+    // for(int i=1; i<=n; i++){
+    //     for(int k=n; k>i; k--){
+    //         cout<<" ";
+            
+    //     }
+    //     for(int j=1; j<=i; j++){
+    //         cout<<j;
+    //     }
+    //     for(int l=1; l<i; l++){
+    //         cout<<l;
+    //     }
+    //     cout<<endl;
+    // }
+    
+    //Method 2
+    for(int i=1; i<=n; i++){
+        for(int k=n; k>i; k--){
+            cout<<" ";
+            
+        }
+        int l=i-1;
+        for(int j=1; j<2*i; j++){
+            if(j<=i){
+                cout<<j;
+            }
+            else{
+                cout<<l;
+                l--;
+            }
+
+        }
+        cout<<endl;
+    }
+}
