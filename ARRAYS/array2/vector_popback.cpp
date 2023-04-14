cd03adf942937988fef594ac372c759529fa6847
suraj-78 <surajpandit591@gmail.com>
2023-04-15 05:19:33 +0530

    vector_popback

---
+++ ARRAYS/array2/vector_popback.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(7);
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"Capacity is :"<<v.capacity()<<endl;
+    v.push_back(3);
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"Capacity is :"<<v.capacity()<<endl;
+    v.push_back(8);
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"Capacity is :"<<v.capacity()<<endl;
+    v.push_back(5);
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"Capacity is :"<<v.capacity()<<endl;
+    
+    v.pop_back();
+    v.pop_back();
+    cout<<"size is :"<<v.size()<<endl;
+    cout<<"Capacity is :"<<v.capacity()<<endl;
+
+
+} 
